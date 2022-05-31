d349c05fe26373515cf5769bf5d4ce985de00958
Yevheniia P <90108689+Yivgha@users.noreply.github.com>
2022-05-31 15:31:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
-import { ApiService } from './js/Api';
+import { ApiService } from './js/аpi';
 import { getRefs } from './js/getRef';
 import { checkRequest } from './js/check-request';
 import { renderMarkup } from './js/renderingMarkup';
